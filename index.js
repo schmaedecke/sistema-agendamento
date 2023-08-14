f1cf653764088c18ab712659b09f760654f4f915
Gustavo Luis Schmaedecke <97gustavo.luis@gmail.com>
2023-08-14 18:32:26 -0300

    Formulário de cadastro

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set("view enngine", "ejs");
+app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/sistemaconsultorio");
+
+app.get("/cadastro", (req, res) => {
+  res.render("create");
+});
 
 app.get("/", (req, res) => {
   res.send("OI!");
